38bf188c75daa2876612bfefb4957ecbdf131ce6
jackSparrow381 <developerasad381@gmail.com>
2022-05-01 16:52:22 +0500

    Minor Changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { StopWatch } from "./Main/StopWatch";
+import {Main} from "./Main/Style";
+
+function App() {
+  return (
+    <Main>
+      <StopWatch />
+    </Main>
+  );
+}
+
+export default App;
